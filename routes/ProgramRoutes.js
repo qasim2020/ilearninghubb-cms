b5b7ffceb1ab9ddf5892341c17933ccb5924933f
Qasim Ali <qasimali24@gmail.com>
2025-12-23 16:58:49 +0100

    successfully deletes previously uploaded image

--- routes/ProgramRoutes.js
+++ routes/ProgramRoutes.js
@@ -1,4 +1,5 @@
 const multer = require("multer");
+const fs = require("fs");
 const path = require("path");
 
 const express = require('express');
@@ -22,6 +23,20 @@ router.post("/upload-program-image", upload.single("programImage"), (req, res) =
     if (!req.file) return res.status(400).json({ error: "No file uploaded" });
     // Return the uploaded image URL
     res.json({ imageUrl: `/uploads/${req.file.filename}` });
+});
+
+router.post("/delete-program-image", (req, res) => {
+    const imageUrl = req.body.imageUrl; 
+    if (!imageUrl) return res.status(400).send("No image URL provided");
+
+    const filePath = path.join(__dirname, "..", imageUrl);
+    fs.unlink(filePath, (err) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send("Failed to delete file");
+        }
+        res.send({ message: "Deleted successfully" });
+    });
 });
 
 router.get("/programs", requireLogin, programController.programs);
